3b28ddb347f066149f7162ab14f92a962de03061
nosoosso <nosoosso.null@gmail.com>
2019-01-04 00:12:14 +0900

    Add prettier

--- js/index.js
+++ js/index.js
@@ -1,20 +1,29 @@
-const rust = import('./rust/katatema.js')
+const rust = import("./rust/katatema.js");
+
+const fps = 30;
+let now = 0;
 
 const gameLoop = rust => tFrame => {
-  window.requestAnimationFrame(gameLoop(rust))
+  let delta = Math.floor(tFrame - now);
 
-  rust.update()
+  while (delta >= 0) {
+    delta -= 1000.0 / fps;
+  }
+
+  rust.update();
+
+  window.requestAnimationFrame(gameLoop(rust));
 };
 
 rust
   .then(rust => {
-    window.document.onkeydown = (ev => {
-      rust.send_browser_key_event(ev.key)
-    });
+    window.document.onkeydown = ev => {
+      rust.send_browser_key_event(ev.key);
+    };
 
     rust.init();
     gameLoop(rust)();
   })
   .catch(err => {
-    console.error(err)
+    console.error(err);
   });
